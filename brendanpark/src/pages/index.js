779e7521540e3b81734df71dfe3cb1395c572b13
maceacherndjh <maceacherndjh@gmail.com>
2020-02-20 20:57:53 +0000

    Remove cruft.

--- brendanpark/src/pages/index.js
+++ brendanpark/src/pages/index.js
@@ -2,19 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Fasten your headphones, for a journey through sound...</p>
+    <p>... coming soon...</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
