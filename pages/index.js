84d4ebb8c859ec30c1af5a53ede580735bc7eb95
David MacEachern <david.maceachern.tech@gmail.com>
2020-12-09 18:02:58 +0100

    Add a squint menu

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import Head from 'next/head'
 import Image from 'next/image'
-import { bgWrap } from '../styles.module.css'
+import { bgWrap, title, titleLink, siteHeader, headerNav, navItems, navItem, navLink, headerWrapper } from '../styles.module.css'
 
 const Index = () => (
   <div>
     <div className={bgWrap}>
+      <Head>
+        <title>Brendan Park</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Image
         alt="Frost covered field"
         src="/background.jpg"
@@ -11,8 +16,32 @@ const Index = () => (
         objectFit="cover"
         quality={100}
       />
+      <header className={siteHeader}>
+        <div className={headerWrapper}>
+          <div className={title}>
+            <a href="https://brendanpark.co.uk/" rel="home" className={titleLink}>Brendan Park</a>
+          </div>
+          <nav className={headerNav}>
+            <ul className={navItems}>
+              <li className={navItem}>
+                <a className={navLink} href="https://soundcloud.com/flaudiolab">Sound</a>
+              </li>
+              <li className={navItem}>
+                <a className={navLink} href="https://www.youtube.com/channel/UCATH813AjPo_VLungWByWgg">Video</a>
+              </li>
+              <li className={navItem}>
+                <a className={navLink} href="https://www.instagram.com/brendan_park93/">Photo</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     </div>
   </div>
 )
 
 export default Index
+// TODO
+// add font modern sans or Microsoft Jheng Hei UI Light
+// css align menu items
+// css hover effect
