f66b53324093d7a9f1f4b2060ff8bdec2bdd7aaa
David MacEachern <david.maceachern.tech@gmail.com>
2020-12-09 14:22:17 +0100

    use new image optimization feature in nextjs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const withPlugins = require('next-compose-plugins')
-const nextImg = require('next-img/plugin')
- 
-module.exports = withPlugins([
-  nextImg,
-  {
-    // specify the default breakpoints
-    breakpoints: [768],
+module.exports = {
+  images: {
+    domains: ['assets.vercel.com'],
   },
-])
+}
