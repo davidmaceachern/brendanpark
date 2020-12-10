490dbc3016dadccedd746619e9b519e34f6093d0
David MacEachern <david.maceachern.tech@gmail.com>
2020-12-10 10:33:20 +0100

    update import

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { bgWrap, title, titleLink, siteHeader, headerNav, navItems, navItem, navLink, headerWrapper } from '../styles.module.css'
+import styles from '../styles.module.css'
 
 const Index = () => (
   <div>
-    <div className={bgWrap}>
+    <div className={styles.bgWrap}>
       <Head>
         <title>Brendan Park</title>
         <link rel="icon" href="/favicon.ico" />
@@ -16,25 +16,25 @@ const Index = () => (
         objectFit="cover"
         quality={100}
       />
-      <header className={siteHeader}>
-        <div className={headerWrapper}>
-          <div className={title}>
-            <a href="https://brendanpark.co.uk/" rel="home" className={titleLink}>Brendan Park</a>
+      <header className={styles.siteHeader}>
+        <div className={styles.headerWrapper}>
+          <div className={styles.title}>
+            <a href="https://brendanpark.co.uk/" rel="home" className={styles.titleLink}>Brendan Park</a>
           </div>
-          <nav className={headerNav}>
-            <ul className={navItems}>
-              <li className={navItem}>
-                <a className={navLink} href="https://soundcloud.com/flaudiolab">Sound</a>
+          </div>
+          <nav className={styles.headerNav}>
+            <ul className={styles.navItems}>
+              <li className={styles.navItem}>
+                <a className={styles.navLink} href="https://soundcloud.com/flaudiolab">Sound</a>
               </li>
-              <li className={navItem}>
-                <a className={navLink} href="https://www.youtube.com/channel/UCATH813AjPo_VLungWByWgg">Video</a>
+              <li className={styles.navItem}>
+                <a className={styles.navLink} href="https://www.youtube.com/channel/UCATH813AjPo_VLungWByWgg">Video</a>
               </li>
-              <li className={navItem}>
-                <a className={navLink} href="https://www.instagram.com/brendan_park93/">Photo</a>
+              <li className={styles.navItem}>
+                <a className={styles.navLink} href="https://www.instagram.com/brendan_park93/">Photo</a>
               </li>
             </ul>
           </nav>
-        </div>
       </header>
     </div>
   </div>
@@ -42,6 +42,6 @@ const Index = () => (
 
 export default Index
 // TODO
-// add font modern sans or Microsoft Jheng Hei UI Light
-// css align menu items
-// css hover effect
+// add font:  modern sans or Microsoft Jheng Hei UI Light
+// css align title with navbar 
+// react three fiber effect 
